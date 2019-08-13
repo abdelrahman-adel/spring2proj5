e6b191d249c290a6400dec4ec1f83de00a7f22d2
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-13 16:59:31 +0200

    first

---
+++ src/main/java/com/master/spring/spring2proj5/main/Spring2proj5Application.java
@@ -0,0 +1,13 @@
+package com.master.spring.spring2proj5.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2proj5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2proj5Application.class, args);
+	}
+
+}
